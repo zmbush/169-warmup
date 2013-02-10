d7e2f055a0430731ca7e8f67339adc5edbb4c82c
Zachary Bush <zmbush@berkeley.edu>
2013-02-10 00:27:45 -0800

    Add initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warmup::Application.config.secret_token = 'fecdd8ce7efb2912acfb692a73654e343735f3a6a9dfaf8e1d8c171ea3ee9b8018f00f71e9dd92c7194ce3f89554b5658aa0dd543e75c6567da47be49e9a1437'
