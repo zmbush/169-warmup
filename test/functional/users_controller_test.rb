55b55da8f0b5e033bb7a68f6e2c8dd9239cac424
Zachary Bush <zmbush@berkeley.edu>
2013-02-12 11:50:31 -0800

    Update functional tests and unit tests

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -11,4 +11,48 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "login zach" do
+    post :login, :user => 'zach', :password => 'bush'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::SUCCESS, body["errCode"], "Couldn't log in as zach"
+    assert_equal 101,  body["count"], "Zach's login count was wrong"
+  end
+
+  test "login hayg" do
+    post :login, :user => 'hayg', :password => 'astourian'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::SUCCESS, body["errCode"], "Couldn't log in as hayg"
+    assert_equal 51,  body["count"], "Hayg's login count was wrong"
+  end
+
+  test "add user" do
+    post :add, :user => 'zach1', :password => 'bush'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::SUCCESS, body["errCode"], "couldn't add user"
+    assert_equal 1, body["count"], "login count for new user is wrong"
+
+    post :login, :user => 'zach1', :password => 'bush'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::SUCCESS, body["errCode"], "couldn't log in with new user"
+    assert_equal 2, body["count"], "login count was wrong"
+  end
+
+  test "try to recreate zach" do
+    post :add, :user => 'zach', :password => 'bush'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::ERR_USER_EXISTS, body["errCode"], "was able to create user again!"
+  end
+
+  test "try to recreate hayg" do
+    post :add, :user => 'hayg', :password => 'astourian'
+    assert_response :success
+    body = JSON.parse(@response.body)
+    assert_equal Users::ERR_USER_EXISTS, body["errCode"], "was able to create user again!"
+  end
 end
+
